94679a45d00ae91c2ab61df735a3e90cf5b4609f
edwardyangey <wangsh@wangshtekiiMac.local>
2013-10-13 22:34:37 +0800

    Initial Commit

---
+++ DragDropFileShare/dragdropshareAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  dragdropshareAppDelegate.h
+//  DragDropFileShare
+//
+//  Created by wangsh on 13-10-13.
+//  Copyright (c) 2013年 wangsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dragdropshareAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
